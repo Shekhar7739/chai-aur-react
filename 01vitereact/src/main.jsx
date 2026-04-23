1f98eb01cc2a880b713b90263072a688144d4b44
Shekhar7739 <codewithshekhar9596@gmail.com>
2026-04-23 20:24:58 +0530

    main

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -25,8 +25,14 @@ const anotherElement = (
     Visit google
   </a>
 );
+
+const reactElement = React.createElement(
+  'a',
+  {href: "https://www.google.com", target: "_blank"},
+  'Click me to visit google'
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-   anotherElement
+   <App />
 
 )
