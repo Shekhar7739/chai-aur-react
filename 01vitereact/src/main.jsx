9db6d597fa12e571682d757251a1b6021d487750
Shekhar7739 <codewithshekhar9596@gmail.com>
2026-04-24 18:12:36 +0530

    main

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,40 +1,10 @@
 import React  from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App.jsx'
+import './index.css'
 
-function MyApp() {
-  return (
-    <div>
-      <h1>Custom App</h1>
-    </div>
-  );
-}
-
-const ReactElement = {
-  type: "a",
-  props: {
-    href: "https://www.google.com",
-    target: "_blank",
-  },
-  Children: "Click me to visit google",
-};
-
-const anotherElement = (
-  <a href="https://www.google.com" target="_blank">
-    Visit google
-  </a>
-);
-
-const anotherUser = "Chai aur Code"
-const reactElement = React.createElement(
-  'a',
-  {href: "https://www.google.com", target: "_blank"},
-  'Click me to visit google',
-  anotherElement
-)
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-   reactElement
-
+  <React.StrictMode>
+   <App />
+  </React.StrictMode>
 )
